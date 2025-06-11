3d3abc399f45bdc8a65e0678b016acb109e4e623
Hassnainabbasi <168479142+Hassnainabbasi@users.noreply.github.com>
2025-06-11 23:46:32 +0500

    Add

--- api/index.js
+++ api/index.js
@@ -1,37 +1,37 @@
-const express = require("express")
-const cookieParser = require('cookie-parser')
-const app = express()
-const dotenv = require('dotenv').config()
-const connectDb = require('../config/db')
-const roomRoutes = require('../routes/roomates')
-const bookingRoutes = require('../routes/bookingRoutes')
-const userRoutes = require('../routes/userRoutes')
-const { errormiddleware } = require("../middleware/errormiddleware")
-const { auth } = require("../middleware/auth")
-const cors = require('cors')
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
+const dotenv = require("dotenv").config();
+const connectDb = require("../config/db");
+const roomRoutes = require("../routes/roomates");
+const bookingRoutes = require("../routes/bookingRoutes");
+const userRoutes = require("../routes/userRoutes");
+const { errormiddleware } = require("../middleware/errormiddleware");
+const { auth } = require("../middleware/auth");
+const cors = require("cors");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://hotel-booking-frontend-smoky.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-  
 
-app.use('/api/rooms', roomRoutes)
+app.use("/api/rooms", roomRoutes);
 app.use("/api/booking", bookingRoutes);
 app.use("/api/user", userRoutes);
 app.use("/auth", auth);
 
+app.use(errormiddleware);
+connectDb();
 
-
-app.use(errormiddleware)
-connectDb()
-
-app.listen(port, ()=> console.log(`listing on Port${port}`))
+app.listen(port, () => console.log(`listing on Port${port}`));
